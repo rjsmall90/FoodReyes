4a1f60d1af8c1fd3a087e0561963ba0aa8720b37
rjsmall90 <rjsmall90@gmail.com>
2019-04-11 13:38:22 -0400

    item class addded

---
+++ menu/src/main/java/com/foodreyes/menu/Model/Item.java
@@ -0,0 +1,46 @@
+package com.foodreyes.menu.Model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "item")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    String name;
+    Double price;
+
+    public Item(Long id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
